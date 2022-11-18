557795a9e99765e77158b09ef5abaf77d78a2a43
vijeayan <vijeayanvj@gmail.com>
2022-11-18 21:35:45 +0530

    f2

--- frontend/src/screens/OrderHistoryScreen.js
+++ frontend/src/screens/OrderHistoryScreen.js
@@ -6,7 +6,7 @@ import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { Store } from '../Store';
 import { getError } from '../utils';
-import { Button } from 'bootstrap';
+import Button from 'react-bootstrap/esm/Button';
 
 const reducer = (state, action) => {
   switch (action.type) {
